626c47dd5143c2f992e174b0c6d43bcf1e912a22
“horacenjoroge” <horacenjorge@gmail.com>
2025-01-21 02:53:38 +0300

    add app.js

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { Provider } from 'react-redux';
+import MainComponent from './screens/MainComponent';
+// import store from './src/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Moringa Connect!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <MainComponent />
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
