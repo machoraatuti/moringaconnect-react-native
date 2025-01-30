21f0f8557867990d536fd4c3b2e270fe44a3a31f
Ngure Kibaara <ngurekibaara01@gmail.com>
2025-01-30 13:06:03 +0300

    Add SplashScreen

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 // import { Provider } from 'react-redux';
 import MainComponent from './screens/MainComponent';
 // import store from './src/store';
 
 export default function App() {
+
+  const [appReady, setAppReady] = useState(false);
+
+  useEffect(() => {
+    const prepare = async() => {
+      try{
+        await SplashScreen.preventAutoHideAsync();
+
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      } finally{
+        setAppReady(true);
+      }
+    };
+    
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if(appReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appReady]);
+
   return (
     <>
       <NavigationContainer>
