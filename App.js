95983804bef107240c810fcdf9844dcdd37c163f
“horacenjoroge” <horacenjorge@gmail.com>
2025-01-31 16:16:00 +0300

    debug app.js

--- App.js
+++ App.js
@@ -1,73 +1,62 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { StatusBar } from 'expo-status-bar';
 import { View, ActivityIndicator } from 'react-native';
 import { store, persistor } from './Redux/store';
-import React, { useEffect, useState } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
-// import { Provider } from 'react-redux';
 import MainComponent from './screens/MainComponent';
 import { checkAuthToken } from './features/authSlice/authSlice';
-import { useDispatch } from 'react-redux';
 
 const LoadingScreen = () => (
- <View style={{ 
-   flex: 1, 
-   justifyContent: 'center', 
-   alignItems: 'center', 
-   backgroundColor: '#0A1F44' 
- }}>
-   <ActivityIndicator size="large" color="#F05A28" />
- </View>
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#0A1F44' }}>
+    <ActivityIndicator size="large" color="#F05A28" />
+  </View>
 );
 
 const AppContent = () => {
- const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
- useEffect(() => {
-   dispatch(checkAuthToken());
- }, [dispatch]);
+  useEffect(() => {
+    dispatch(checkAuthToken());
+  }, [dispatch]);
 
- return (
-   <NavigationContainer>
-     <MainComponent />
-     <StatusBar style="light" />
-   </NavigationContainer>
- );
+  return (
+    <NavigationContainer>
+      <MainComponent />
+      <StatusBar style="light" />
+    </NavigationContainer>
+  );
 };
 
 export default function App() {
-
   const [appReady, setAppReady] = useState(false);
 
   useEffect(() => {
-    const prepare = async() => {
-      try{
+    const prepare = async () => {
+      try {
         await SplashScreen.preventAutoHideAsync();
-
         await new Promise(resolve => setTimeout(resolve, 5000));
-      } finally{
+      } finally {
         setAppReady(true);
       }
     };
-    
+
     prepare();
   }, []);
 
   useEffect(() => {
-    if(appReady) {
+    if (appReady) {
       SplashScreen.hideAsync();
     }
   }, [appReady]);
 
   return (
-    <>
-      <NavigationContainer>
-        <MainComponent />
-        <StatusBar style="auto" />
-      </NavigationContainer>
-    </>
+    <Provider store={store}>
+      <PersistGate loading={<LoadingScreen />} persistor={persistor}>
+        {appReady ? <AppContent /> : <LoadingScreen />}
+      </PersistGate>
+    </Provider>
   );
 }
